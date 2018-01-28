92915f3e5b6dd636929004d30ca0e1dad452df88
otmichael <ozmichaelt@gmail.com>
2018-01-27 17:17:16 -0800

    Add base website

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+	$('.content-box a').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom',
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300
+		}
+	});
+
+});
